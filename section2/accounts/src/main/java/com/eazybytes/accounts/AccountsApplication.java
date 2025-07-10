3ba75ffdb7ade581a76e4699989d51c93778d877
premvinodh <premvinodh.pc@gmail.com>
2025-07-10 13:46:39 +0530

    Section 2: Accounts - Annotations to be used with non-standard packaging for controllers, etc.

--- section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+
+// Explicitly tell SpringBootApplication the location of the controllers, etc
+// if they are not in the same package as this class
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
+
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
 

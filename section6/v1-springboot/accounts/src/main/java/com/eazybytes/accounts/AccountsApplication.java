4913b2b429df7fa70bcf1ea03a5c2fcc04d77504
premvinodh <premvinodh.pc@gmail.com>
2025-08-12 21:39:00 +0530

    Section 6: Accounts - @ConfigurationProperties annotation

--- section6/v1-springboot/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section6/v1-springboot/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts;
 
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -17,6 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaRepositories("com.eazybytes.accounts.repository")
 @EntityScan("com.eazybytes.accounts.model")*/
 
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
 		info = @Info(
